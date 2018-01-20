cdd03d6fdd2bac472afc2bb768473d9b87d2f699
hackboxlive <singhsarvshakti127@gmail.com>
2018-01-21 03:57:44 +0530

    Added CMakeLists.txt for build

---
+++ CMakeFiles/impulse.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/impulse.dir/src/main.cpp.o"
+  "CMakeFiles/impulse.dir/src/neuron.cpp.o"
+  "impulse.pdb"
+  "impulse"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/impulse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
